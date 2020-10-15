6f533affce28b43687a4a14f78fb408a10f5cd29
Eytan Singher <eytan.singher@gmail.com>
2020-10-15 19:40:37 +0300

    Fix for the functon +2

--- frontend/th_to_forall.py
+++ frontend/th_to_forall.py
@@ -32,6 +32,8 @@ def to_str_no_qm(sexp):
             return sexp.value()[1:]
         return sexp.value()
     if not isinstance(sexp, list):
+        if isinstance(sexp, int) and sexp > 0:
+            return {'+' + str(sexp)}
         return str(sexp)
     children = [to_str_no_qm(c) for c in sexp]
     return f"({' '.join(children)})"
