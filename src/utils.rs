14f65dfeb6ba83f94fb48cf026246680f919f051
Omer Moreno <omer.moreno13@gmail.com>
2020-12-03 14:15:31 +0200

    Added utils for testing\debugging

---
+++ src/utils.rs
@@ -0,0 +1,16 @@
+/// a macro used for debugging
+/// prints only when running tests
+#[proc_macro]
+pub(crate) fn test_print(str: &String){
+    if cfg!(test){
+        println!("{}", str);
+    }
+}
+
+/// a macro for calling functions only when testing
+/// In our use, good for exporting dot files to track changes in e-graph
+pub(crate) fn test_call_fn(f: &dyn Fn()->()){
+    if cfg!(test){
+        f();
+    }
+}
