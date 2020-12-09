2d84d3c72b73d2076e6b42e1f09e1c13ac39d236
Omer Moreno <omer.moreno13@gmail.com>
2020-12-09 19:23:22 +0200

    adde HookData and SenderReceiver structs to parallel.rs

--- src/thesy/parallel.rs
+++ src/thesy/parallel.rs
@@ -1,6 +1,6 @@
 use alloc::boxed::Box;
 use egg::{Rewrite, SymbolLang};
-
+use std::sync::mpsc;
 
 /// An enum used for passing messages between threads when running Thesy in parallel mode
 pub enum Message{
@@ -8,3 +8,16 @@ pub enum Message{
     Rule(Box<Rewrite<SymbolLang,()>>),                              // intended for sending a rule from main thread to all other threads, as there is no id needed in this case
     Done(usize),                                                        // not sure if needed, but is used when one thread has done running
 }
+
+/// Used for passing data to hooks
+pub struct HookData<'a>{
+    pub new_rules: &'a [Rewrite<SymbolLang, ()>],
+}
+
+// Used in TheSy instances during parallel run
+pub struct SenderReceiverWrapper<ReceiveType, SendType>{
+    pub tx: mpsc::Sender<ReceiveType>,
+    pub rc: mpsc::Receiver<SendType>,
+}
+// The standard case of SenderReceiverWrapper
+pub type MsgSenderReceiverWrapper = SenderReceiverWrapper<Box<Message>,Box<Message>>;
