aaa85e51dfa65feab9bc3b5de9b40d8e697e0152
eytans <eytan.singher@gmail.com>
2023-03-29 13:43:18 +0300

    Fixed smt -> th translator versioning + output processing

--- frontend/smtlib/main.py
+++ frontend/smtlib/main.py
@@ -15,7 +15,7 @@ def main():
     parser = ArgumentParser(description='Extract thesy from SMT-LIB files')
     parser.add_argument('smtlib_in', nargs='*', help='SMT-LIB file or directory to extract thesy from (default: stdin)')
     parser.add_argument('--output', '-o', help='Output directory according to input (default: stdout)')
-    parser.add_argument('--thesy_version', '-v', help='Thesy version to use (default: 2)', choices=[1, 2], default=2)
+    parser.add_argument('--thesy_version', '-v', help='Thesy version to use (default: 2)', choices=["1", "2"], default="2")
     parser.add_argument('--verbose', type=bool, default=True)
     args = parser.parse_args()
 
@@ -35,9 +35,9 @@ def main():
         with fn.open('r') as infile:
             reset_env()
             try:
-                if args.thesy_version == 1:
+                if args.thesy_version == "1":
                     doc = ThesyFromSmt(infile)
-                elif args.thesy_version == 2:
+                elif args.thesy_version == "2":
                     doc = NewThesyFromSmt(infile)
                 else:
                     raise NotImplementedError("Only support to thesy formats")
@@ -48,7 +48,9 @@ def main():
 
         out = stdout
         if args.output:
-            out = open(Path(args.output) / fn.relative_to(d).with_suffix('.th'), 'w')
+            fp = Path(args.output) / fn.relative_to(d).with_suffix('.th')
+            fp.parent.mkdir(parents=True, exist_ok=True)
+            out = fp.open('w')
 
         try:
             for el in doc:
